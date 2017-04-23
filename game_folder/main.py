ff4487643b347551d7300628f09ebef4e1b46366
Marcus Joseph Schimizzi <mschimiz@nd.edu>
2017-04-23 19:11:19 -0400

    fixed a few things

--- game_folder/main.py
+++ game_folder/main.py
@@ -80,12 +80,6 @@ class Game:
 				if event.key == pg.K_ESCAPE:
 					self.quit()
 
-	def show_start_screen(self):
-		pass
-
-	def show_go_screen(self):
-		pass
-
 # create game object
 g = Game()
 g.show_start_screen()
