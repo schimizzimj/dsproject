5bf0515bbddba2a6c1b17c57211195e2f94dd9e7
Abigail Shirey <ashirey@nd.edu>
2017-05-05 03:53:33 -0400

    done

--- game_folder/emrichscene.py
+++ game_folder/emrichscene.py
@@ -11,31 +11,35 @@ import dialogue
 
 # ordering data structures horizontally
 class DataStructures(scene.Scene):
-    def __init__(self, director):
+    def __init__(self, director, game, post_win):
+        scene.Scene.__init__(self, director)
+        self.game = game
         # make screen
         pg.init()
         self.screen = pg.display.set_mode((WIDTH, HEIGHT), 0, 32)
         pg.display.set_caption("Data Structures")
-        font = pg.font.Font(pg.font.get_default_font(), 12)
+        self.font = pg.font.Font(pg.font.get_default_font(), 16)
         self.screen.fill(WHITE)
 
         # set vars
+	self.changed = True
+
         # width, height
-        self.w = 150
+        self.w = 200
         self.h = 50
         # mouse vars
         self.pressed = False
         self.drag = False
         # height of first rectangle
-        self.hi = HEIGHT/4-w/2
+        self.hi = HEIGHT/3-self.w/2
 
         # rectangle vars
-        self.xBST = WIDTH/2-w/2
-        self.yBST = hi
-        self.xLL = WIDTH/2-w/2
-        self.yLL = hi + 3*h
-        self.xA = WIDTH/2-w/2
-        self.yA = hi + 6*h
+        self.xBST = WIDTH/2-self.w/2
+        self.yBST = self.hi
+        self.xLL = WIDTH/2-self.w/2
+        self.yLL = self.hi + 3*self.h
+        self.xA = WIDTH/2-self.w/2
+        self.yA = self.hi + 6*self.h
 
         # ellipsis vars
         self.xe = 0
@@ -58,47 +62,55 @@ class DataStructures(scene.Scene):
             self.pressed = True
             self.coords = pg.mouse.get_pos()
             # drag bst
-            if self.xBST < coords[0] < self.xBST + self.w and self.yBST < coords[1] < self.yBST + self.h:
-                self.xBST = coords[0] - self.w/2
-                self.yBST = coords[1] - self.h/2
+            if self.xBST < self.coords[0] < self.xBST + self.w and self.yBST < self.coords[1] < self.yBST + self.h:
+		self.changed = True
+                self.xBST = self.coords[0] - self.w/2
+                self.yBST = self.coords[1] - self.h/2
             # drag linked list
-            elif self.xLL < coords[0] < self.xLL + self.w and self.yLL < coords[1] < self.yLL + self.h:
-                self.xLL = coords[0] - self.w/2
-                self.yLL = coords[1] - self.h/2
+            elif self.xLL < self.coords[0] < self.xLL + self.w and self.yLL < self.coords[1] < self.yLL + self.h:
+		self.changed = True
+                self.xLL = self.coords[0] - self.w/2
+                self.yLL = self.coords[1] - self.h/2
             # drag array
-            elif self.xA < coords[0] < self.xA + self.w and self.yA < coords[1] < self.yA + self.h:
-                self.xA = coords[0] - self.w/2
-                self.yA = coords[1] - self.h/2
+            elif self.xA < self.coords[0] < self.xA + self.w and self.yA < self.coords[1] < self.yA + self.h:
+		self.changed = True
+                self.xA = self.coords[0] - self.w/2
+                self.yA = self.coords[1] - self.h/2
             # press button to check order
-            elif self.xe < coords[0] < self.xe + self.we and self.ye < coords[1] < self.ye + self.he:
-                endGame()
+            elif self.xe < self.coords[0] < self.xe + self.we and self.ye < self.coords[1] < self.ye + self.he:
+                self.endGame()
         # stop dragging box
         elif event.type == pg.MOUSEBUTTONUP:
             self.pressed = False
 
     def update(self):
         if self.pressed:
-            holding(self)
+            self.holding()
             # drag bst
-            if self.xBST < coords[0] < self.xBST + self.w and self.yBST < coords[1] < self.yBST + self.h:
-                self.xBST = coords[0] - self.w/2
-                self.yBST = coords[1] - self.h/2
+            if self.xBST < self.coords[0] < self.xBST + self.w and self.yBST < self.coords[1] < self.yBST + self.h:
+		self.changed = True
+                self.xBST = self.coords[0] - self.w/2
+                self.yBST = self.coords[1] - self.h/2
             # drag linked list
-            elif self.xLL < coords[0] < self.xLL + self.w and self.yLL < coords[1] < self.yLL + self.h:
-                self.xLL = coords[0] - self.w/2
-                self.yLL = coords[1] - self.h/2
+            elif self.xLL < self.coords[0] < self.xLL + self.w and self.yLL < self.coords[1] < self.yLL + self.h:
+		self.changed = True
+                self.xLL = self.coords[0] - self.w/2
+                self.yLL = self.coords[1] - self.h/2
             # drag array
-            elif self.xA < coords[0] < self.xA + self.w and self.yA < coords[1] < self.yA + self.h:
-                self.xA = coords[0] - self.w/2
-                self.yA = coords[1] - self.h/2
+            elif self.xA < self.coords[0] < self.xA + self.w and self.yA < self.coords[1] < self.yA + self.h:
+		self.changed = True
+                self.xA = self.coords[0] - self.w/2
+                self.yA = self.coords[1] - self.h/2
 
-    def render(self, screen):
-        self.screen.fill(WHITE)
-        self.drawHeader()
-        self.drawButton()
-        self.drawBSTRect()
-        self.drawLLRect()
-        self.drawARect()
+    def render(self):
+        if self.changed:
+		self.screen.fill(WHITE)
+        	self.drawHeader()
+        	self.drawButton()
+        	self.drawBSTRect()
+        	self.drawLLRect()
+        	self.drawARect()
+		self.changed = False
 
     def drawButton(self):
         #global xe, ye, he, we
@@ -107,29 +119,29 @@ class DataStructures(scene.Scene):
         self.we = length/2
         self.xe = WIDTH/2 - length/2
         self.ye = 4*HEIGHT/5
-        pg.draw.ellipse(screen, (0,255,0), pg.Rect(self.xe, self.ye, length, length/2))
-        screen.blit(font.render("Done", True, BLACK), (WIDTH/2-length/4, 4*HEIGHT/5+length/6))
+        pg.draw.ellipse(self.screen, (0,255,0), pg.Rect(self.xe, self.ye, length, length/2))
+        self.screen.blit(self.font.render("Done", True, BLACK), (WIDTH/2-length/4, 4*HEIGHT/5+length/6))
 
     def drawBSTRect(self):
         #global xBST, yBST
-        rect = pg.draw.rect(screen, TEAL, pg.Rect(self.xBST, self.yBST, self.w, self.h))
+        rect = pg.draw.rect(self.screen, TEAL, pg.Rect(self.xBST, self.yBST, self.w, self.h))
         pg.display.update()
-        addText(self.xBST, self.yBST, self.h, "Binary Search Tree")
+        self.addText(self.xBST+10, self.yBST, self.h, "Binary Search Tree")
 
     def drawLLRect(self):
         # global xLL, yLL
-        rect = pg.draw.rect(screen, TEAL, pg.Rect(self.xLL, self.yLL, self.w, self.h))
+        rect = pg.draw.rect(self.screen, TEAL, pg.Rect(self.xLL, self.yLL, self.w, self.h))
         pg.display.update()
-        addText(self.xLL, self.yLL, self.h, "Single-Linked List")
+        self.addText(self.xLL+10, self.yLL, self.h, "Single Linked List")
 
     def drawARect(self):
         # global xA, yA
-        rect = pg.draw.rect(screen, TEAL, pg.Rect(self.xA, self.yA, self.w, self.h))
+        rect = pg.draw.rect(self.screen, TEAL, pg.Rect(self.xA, self.yA, self.w, self.h))
         pg.display.update()
-        addText(self.xA, self.yA, self.h, "Array")
+        self.addText(self.xA+self.w/3, self.yA, self.h, "Array")
 
     def addText(self, xT, yT, h, label):
-        self.screen.blit(font.render(label, True, BLACK), (xT, yT+h/2))
+        self.screen.blit(self.font.render(label, True, BLACK), (xT, yT+h/2))
         pg.display.update()
 
     def holding(self):
@@ -139,14 +151,17 @@ class DataStructures(scene.Scene):
 
     def drawHeader(self):
         # header
-        self.screen.blit(font.render("Drag the data structures in smallest to largest average time complexity of accessing an element) ", True, BLACK), (WIDTH/3, HEIGHT/10))
-        self.screen.blit(font.render("Press the green button when done", True, BLACK), (WIDTH/3, HEIGHT/10+12))
+        self.screen.blit(self.font.render("Drag the data structures in smallest to largest average time complexity of accessing an element.", True, BLACK), (WIDTH+10, HEIGHT/13))
+        self.screen.blit(self.font.render("Put the smallest time complexity closest to the left side of the screen and the largest closest to the right side.", True, BLACK), (WIDTH/7, HEIGHT/13+20))
+	self.screen.blit(self.font.render("*in case you didn't pay attention in class: the order is array, bst, linked list.", True, BLACK), (WIDTH/5, HEIGHT/13+40))
+        self.screen.blit(self.font.render("Press the green button when done.", True, BLACK), (WIDTH/3, HEIGHT/13+60))
 
     def endGame(self):
         self.screen.fill(TEAL)
         bigFont = pg.font.Font(pg.font.get_default_font(), 48)
         if self.xA < self.xBST < self.xLL:
             # correct order
-            screen.blit(bigFont.render("Correct!", True, WHITE), (WIDTH/3, HEIGHT/2))
+            self.screen.blit(bigFont.render("Correct!", True, WHITE), (WIDTH/3, HEIGHT/2))
         else:
-            screen.blit(bigFont.render("Not quite! Try again later", True, WHITE), (WIDTH/3, HEIGHT/2))
+            self.screen.blit(bigFont.render("Not quite! Try again later", True, WHITE), (WIDTH/3, HEIGHT/2))
+	pg.display.update()
